a5b13be0228a0b931233ed106e72e280ba4542ea
David Wu <davidjwu92@gmail.com>
2020-01-23 18:38:32 -0800

    Set up react app

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const {join} = require('path')
+
+const app = express()
+ 
+//middlewares
+app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+ 
+require("./routes")(app)
+
+require('mongoose')
+  .connect(process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost/googlebooksdb')
+  .then(()=>app.listen(process.env.PORT || 3001))
+  .catch(e=>console.error(e))
