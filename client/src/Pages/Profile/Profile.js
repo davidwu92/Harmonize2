3f4b00e03964a9bea7ce09b42eaf9777691d0956
David Wu <davidjwu92@gmail.com>
2020-01-23 20:50:02 -0800

    profile sample created

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -4,12 +4,35 @@ const Profile = () =>{
   return (
     <>
       <div class="container">
-        <div class="col s12 m8 offset-m2 l6 offset-l3">
-          <img class ="circle responsive-img" src="../Pages/Profile/Louise.jpg" alt="Louise"/>
+        <div class="row valign-wrapper">
+          <div class="col s2">
+            {/* PROFILE PIC */}
+            <img
+            class ="circle responsive-img"
+            src="https://preview.redd.it/4bsnuu9wpue11.jpg?width=640&crop=smart&auto=webp&s=212294834cfa17f23e370cbadc655c7d6ca48c95"
+            alt="Louise"/>
+          </div>
+          <div class="col s10">
+            <h3 class="black-text">
+              {/* NAME */}
+              Louise Belcher
+            </h3>
+            <h5 class="grey-text">
+              {/* BIO */}
+              My main instrument is the cup-and-straw. I'm all about that jam sesh life!
+            </h5>
+          </div>        
         </div>
-        <div class="col s12 m8 offset-m2 l6 offset-l3">
-          <h2>Louise Belcher</h2>
+        <div>
+          <p>Embedded Video</p>
+          <p>Embedded Video</p>
+          <p>Embedded Video</p>
+          <p>Embedded Video</p>
+          <p>Embedded Video</p>
+          <p>Embedded Video</p>
         </div>
+
+
       </div>
     </>
   )
