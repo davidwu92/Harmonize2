946c60d0179867f4919bc8e11aa1bbe4a6ba2550
David Wu <davidjwu92@gmail.com>
2020-01-25 12:46:49 -0800

    loginUser API function and loginForm functionality

--- client/src/Components/LoginForm/LoginForm.js
+++ client/src/Components/LoginForm/LoginForm.js
@@ -1,7 +1,28 @@
-import React, { useContext } from 'react'
-// import UserContext from '../../utils/UserContext'
+import React, { useState } from 'react'
+import UserAPI from '../../Utils/UserAPI'
+const {loginUser} = UserAPI
 
 const LoginForm = () => {
+  
+  //using LoginState to track username/password on this page only.
+  const [loginState, setLoginState] = useState({
+    username: '',
+    password: ''
+  })
+  loginState.handleInputChange = (event) => {
+    setLoginState({...loginState, [event.target.name]: event.target.value})
+  }
+  //defining function for LOG IN button.
+  loginState.handleLogin = (event) => {
+    event.preventDefault()
+    console.log(loginState.username + loginState.password)
+    loginUser({
+      username: loginState.username,
+      password: loginState.password
+    })
+    .then(()=>{console.log('You attempted a log in.')})
+    .catch((e)=>console.error(e))
+  }
 
   return (
 
@@ -11,14 +32,14 @@ const LoginForm = () => {
           <div className="card-content green-text">
             <h3>Login</h3>
             <div className="input-field">
-              <input placeholder="Username" type="text" id="username" />
+              <input placeholder="Username" type="text" id="username" name="username" value={loginState.username} onChange={loginState.handleInputChange}/>
               <label htmlFor="username"></label>
             </div>
             <div className="input-field">
-              <input placeholder="Password" type="password" id="password" />
+              <input placeholder="Password" type="password" id="password" name="password" value={loginState.password} onChange={loginState.handleInputChange}/>
               <label htmlFor="password"></label>
             </div>
-            <button id="login" className="btn waves-effect waves-light col s12" type="submit" name="action">Submit
+            <button onClick={loginState.handleLogin} id="login" className="btn waves-effect waves-light col s12" type="submit" name="action">Submit
                 <i className="material-icons right">send</i>
             </button>
             <p>Register Link will go somewhere down here</p>
