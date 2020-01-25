a61d976c9752709e7f62d2f12b404aec0c5ff3cc
David Wu <davidjwu92@gmail.com>
2020-01-25 11:19:49 -0800

    Proxy fix applied

---
+++ client/src/setupProxy.js
@@ -0,0 +1,9 @@
+//client/src/setupProxy.js
+
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+  app.use(
+    proxy(["/"], {target:"http://localhost:3000"})
+  )
+}
