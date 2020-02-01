090fc492ddecaea003ca80fe36773fc16a6147ac
Jawed Ekhlas <jawz07r1@gmail.com>
2020-02-01 12:36:22 -0800

    page views update

--- client/src/Pages/LogIn/LogIn.js
+++ client/src/Pages/LogIn/LogIn.js
@@ -5,7 +5,10 @@ const LogIn = () => {
   return (
     <>
       <div className="container">
-        <LoginForm />
+        <LoginForm />   
+            <div className="loading-img">
+              <img src="https://cdn4.vectorstock.com/i/1000x1000/67/63/3d-tunnel-optical-vortex-twist-wireframe-vector-18036763.jpg" width="100%"/>   
+            </div> 
       </div>
     </>
   )
--- client/src/Pages/Register/Register.js
+++ client/src/Pages/Register/Register.js
@@ -9,6 +9,9 @@ return (
     <div className="container">
       <RegisterForm />
     </div>
+    <div className="loading-img">
+       <img src="https://cdn4.vectorstock.com/i/1000x1000/67/63/3d-tunnel-optical-vortex-twist-wireframe-vector-18036763.jpg" width="100%"/>   
+    </div> 
   </>
 )
 }
