9dc24871ee207e540f454b87c3e1be26816a6283
thejunglife <thejunglife@gmail.com>
2020-02-01 12:42:15 -0800

    made seperate component for link videos

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 //Landing Page
 import React from 'react'
-
+import LinksCards from '../../Components/LinksCards'
 
 const Home = () => {
   return (
     <>
+    <LinksCards />
       <h4 className="center-align">Helping musicians find musicians since 2020.</h4>
       <img id="homeimg" src="https://cdn10.bostonmagazine.com/wp-content/uploads/2015/08/shutterstock_band.jpg" />
     </>
