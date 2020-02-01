d6267e6585720c02186865da9822864e7e82f5ca
Jawed Ekhlas <jawz07r1@gmail.com>
2020-02-01 11:06:17 -0800

    finished landing page

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <h4 className="center-align">Helping musicians find musicians since 2020.</h4>
+      <h5 className="center-align">Helping musicians find musicians since 2020.</h5>
       <img id="homeimg" src="https://cdn10.bostonmagazine.com/wp-content/uploads/2015/08/shutterstock_band.jpg" />
       <div className="row">
         <div className="col s12">           
@@ -14,7 +14,7 @@ const Home = () => {
                  Welcome! 
                </h5> 
                 I always encourage musicians to play with others as much as possible.  However, it can feel a bit daunting to find musicians, jam buddies or band members in your area.
-                Don't worry! There are always bands and musicians of all levels looking for someone to jam or collaborate with near you. All you have to do is find them with Harmonize!
+                Don't worry! There are always bands and musicians of all levels looking for someone to jam or collaborate with near you. All you have to do is find them with HARMONIZE!
             </p>             
         </div>
          <div className="col s6">
