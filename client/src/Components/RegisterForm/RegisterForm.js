ff81bdc3f5a1239e123ff6baf70842dfc48e1184
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2020-02-04 12:42:26 -0800

    adding alert to failed sign ins

--- client/src/Components/RegisterForm/RegisterForm.js
+++ client/src/Components/RegisterForm/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import UserContext from '../../utils/UserContext'
 import UserAPI from '../../utils/UserAPI'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 const { addUser } = UserAPI
@@ -9,11 +9,11 @@ const { addUser } = UserAPI
 const RegisterForm = () => {
   const history = useHistory()
 
-  const {name, email, username, password, handleInputChange } = useContext(UserContext)
+  const { name, email, username, password, handleInputChange } = useContext(UserContext)
 
   const handleAddUser = event => {
     event.preventDefault()
-    addUser({ 
+    addUser({
       name,
       email,
       username,
@@ -23,42 +23,48 @@ const RegisterForm = () => {
       bio: `You currently don't have a bio. Click on the edit profile button to tell others about yourself!`,
       pfPic: ``
     })
-    .then(({ data }) => {
-      history.push('/login')
-    })
-    .catch(e => {
-      //ERROR MESSAGE HANDLE
-      console.error(e)
-    })
+      .then(({ data }) => {
+        if (addUser) {
+          history.push('/login')
+        } else {
+          //ALERT MESSAGE 
+          document.getElementById('alertMsg').innerHTML = `
+          *Please enter the correct Info
+          `
+          console.error('Failed to Register')
+        }
+      })
+      .catch(e => console.error(e))
   }
 
   return (
     <div className="row">
       <form id="registerForm" action="" className="col s12">
-            <h3>Register</h3>
-            <div className="input-field">
-              <label htmlFor="name"></label>
-              <input placeholder="Full Name" type="text" id="name" name="name" value={name} onChange={handleInputChange} />
-            </div>
-            <div className="input-field">
-              <input placeholder="Email" type="text" id="email" name="email" value={email} onChange={handleInputChange} />
-              <label htmlFor="email"></label>
-            </div>
-            <div className="input-field">
-              <input placeholder="Username" type="text" id="username" name="username" value={username} onChange={handleInputChange} />
-              <label htmlFor="username"></label>
-            </div>
+        <h3>Register</h3>
+        <div id="alertMsg" className="red-text"></div>
+        <div className="input-field">
+          <label htmlFor="name"></label>
+          <input placeholder="Full Name" type="text" id="name" name="name" value={name} onChange={handleInputChange} />
+        </div>
+        <div className="input-field">
+          <input placeholder="Email" type="text" id="email" name="email" value={email} onChange={handleInputChange} />
+          <label htmlFor="email"></label>
+        </div>
+        <div className="input-field">
+          <input placeholder="Username" type="text" id="username" name="username" value={username} onChange={handleInputChange} />
+          <label htmlFor="username"></label>
+        </div>
 
-            {/* NEED A WAY TO PASS THIS TO BACKEND */}
-            <div className="input-field">
-              <input placeholder="Password" type="password" id="password" name="password" value={password} onChange={handleInputChange} />
-              <label htmlFor="password"></label>
-            </div>
+        {/* NEED A WAY TO PASS THIS TO BACKEND */}
+        <div className="input-field">
+          <input placeholder="Password" type="password" id="password" name="password" value={password} onChange={handleInputChange} />
+          <label htmlFor="password"></label>
+        </div>
 
-              {/* SUBMIT BUTTON */}
-              <button onClick={handleAddUser} id="register" className="btn black waves-effect waves-light col s12" type="submit" name="action">Register
+        {/* SUBMIT BUTTON */}
+        <button onClick={handleAddUser} id="register" className="btn black waves-effect waves-light col s12" type="submit" name="action">Register
                     <i className="material-icons right">send</i>
-              </button>
+        </button>
       </form>
     </div>
   )
