238268963c08f254fe3068b04113e87046ab9ddc
Michelle Gonzalez <m.chellegpedroza@gmail.com>
2020-02-10 20:58:31 -0800

    working on the styling for populated profiles on search

--- client/src/Pages/Search/Search.js
+++ client/src/Pages/Search/Search.js
@@ -47,47 +47,48 @@ const Search = () => {
           <button onClick={handleSearch}><i className="fa fa-search"></i></button>
         </form>
       </div>
+
       <div className="container"> {/* FOUND USERS POPULATE HERE; need styling. */}
         {
           searchState.searchedUsers.length ? searchState.searchedUsers.map((user, index) => (
-            <div className={(index % 2) ? "row grey lighten-4" : "row grey lighten-2"}>
-              <div className="col s6 m6">
-                {/* PROFILE PIC */}
-                {(user.profile) ? <img className="responsive-img" alt="Your pf pic" id="img" src={user.profile} /> : null}
-                  </div>
-              <div className="col s6 m6">
-                {/* BASIC INFO */}
-                <h5 onClick={() => visitProfile(user._id)}>{user.name}</h5>
-                <h6>{user.username}</h6>
-                {/* NEEDS LOCATION */}
+            <div id="pfRow" onClick={() => visitProfile(user._id)} className={(index % 2) ? "row grey lighten-4" : "row grey lighten-2"}>
+              <div className="center-align">
+                <div className="col s4 m4">
+                  {/* PROFILE PIC */}
+                  {(user.profile) ? <img className="circle responsive-img" alt="Your pf pic" id="img" src={user.profile} /> : null}
+                  {/* NAME
+                  <h5>{user.name}</h5> */}
+                  {/* USERNAME */}
+                  <p>{user.username}</p>
                 </div>
-              <div className="col s12 m6 grey lighten-5"> {/* INSTRUMENTS/SKILLS */}
                 {/* INSTRUMENTS */}
-                <div className="col s12 m6">
-                  <h6>Instruments</h6>
+                <div className="col s4 m4">
+                  <h6 className="teal-text">Instruments</h6>
                   {
                     user.instruments.length ? <>
                       {user.instruments.map(instrument => (
-                        <p>{instrument + " "}</p>
+                        <p className="black-text">{instrument + " "}</p>
                       ))}
                     </> : <p>No instruments listed.</p>
                   }
                 </div>
                 {/* SKILLS */}
-                <div className="col s6 m6">
-                  <h6>Skills</h6>
+                <div className="col s4 m4">
+                  <h6 className="teal-text">Skills</h6>
                   {
                     user.skills.length ? <>
                       {user.skills.map(skill => (
-                        <p>{skill + " "}</p>
+                        <p className="black-text">{skill + " "}</p>
                       ))}
                     </> : <p>No skills listed.</p>
                   }
                 </div>
               </div>
             </div>
+
           )) : <p className="white-text center-align">No users match your search query.</p>
         }
+
       </div>
     </>
   )
