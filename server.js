6fa044befcf5baa852b8acd9e7f66beeb4083d92
David Wu <davidjwu92@gmail.com>
2020-02-13 18:53:55 -0800

    comment out 106-108

--- server.js
+++ server.js
@@ -102,11 +102,12 @@ app.post('/', upload.single('img'), passport.authenticate('jwt', { session: fals
 app.get('/:filename', (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
     // Check if file
-    // if (!file || file.length === 0) {
-    //   return res.status(404).json({
-    //     err: 'No file exists',
-    //   })
-    // }
+    if (!file || file.length === 0) {
+      // return res.status(404).json({
+      //   err: 'No file exists',
+      // })
+      return res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+    }
 
     // Check if image
     if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
@@ -114,9 +115,10 @@ app.get('/:filename', (req, res) => {
       const readstream = gfs.createReadStream(file.filename)
       readstream.pipe(res)
     } else {
-      res.status(404).json({
-        err: 'Not an image',
-      })
+      // res.status(404).json({
+      //   err: 'Not an image',
+      // })
+      return res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
     }
   })
 })
