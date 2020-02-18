e4f2b58c522da929758d7c5c627cc88e3e5655f5
David Wu <davidjwu92@gmail.com>
2020-02-18 11:23:34 -0800

    Un-fixed LoggedInNav Friends button

--- client/src/Components/LoggedinNav/LoggedinNav.js
+++ client/src/Components/LoggedinNav/LoggedinNav.js
@@ -40,7 +40,10 @@ const friendsButton = alertState.requests.length ?     <li id="hovEffect" classN
       <div className="nav-content">
         <ul className="tabs tabs-transparent">
           {/* <li id="hovEffect" className="tab left"><Link to="/messages"><i className="fas fa-comment"></i></Link></li> */}
-          <li id="hovEffect" className="tab">{friendsButton}</li>
+          <li id="hovEffect" className="tab">
+            {/* {friendsButton} */}
+            <Link to="/friends"><i className="fas fa-user-friends"></i></Link>
+            </li>
           <li id="hovEffect" className="tab left"><Link to="/gigpostings"><i className="fas fa-guitar"></i></Link></li>
           <li id="hovEffect" className="tab"><Link to="/search"><i className="far fa-compass"></i></Link></li>
           <li id="hovEffect" className="tab"><Link to="/myprofile"><i className="fas fa-user-alt"></i></Link></li>
